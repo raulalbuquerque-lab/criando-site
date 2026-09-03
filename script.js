ab97cd7fdce796aa3ecff791b2b083f0b044d55c
raulalbuquerque-lab <raul.albuquerque@escola.pr.gov.br>
2026-09-03 07:36:24 -0300

    a

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const botao = document.getElementById('botao_tema');
+cconst botao = document.getElementById('botao_tema');
 
-function alternarModo () {
-  document.body.classList.toggle('modo_escuro');
+function alternarModo() {
+  document.body.classList.toggle('modo_claro');
 
-  if(document.body.classList.contains('modo_escuro')) {
-    botao.textContent = 'Modo Claro';
-  }else {
+  if (document.body.classList.contains('modo_claro')) {
     botao.textContent = 'Modo Escuro';
+  } else {
+    botao.textContent = 'Modo Claro';
   }
- 
 }
+
 botao.addEventListener('click', alternarModo);
